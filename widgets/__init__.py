5ab664d3dab9638b393c02b7f7efdd9870188bfb
Guillermo <guillermo.martincoello@gmail.com>
2026-02-20 14:32:38 +0100

    feat(widgets): add Dialog component to widget imports

--- widgets/__init__.py
+++ widgets/__init__.py
@@ -1,1 +1,1 @@
-from ._widgets import Label, Box, Button, TextInput, LogDisplay, ProgressBar, Chart, List, Select, Checkbox, VStack, HStack, Frame
+from ._widgets import Label, Box, Button, TextInput, LogDisplay, ProgressBar, Chart, List, Select, Checkbox, VStack, HStack, Frame, Dialog
