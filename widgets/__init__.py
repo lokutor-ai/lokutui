0dd5bc86b0d1589d95e1903c89e185215b0f40a3
Guillermo <guillermo.martincoello@gmail.com>
2026-02-20 15:33:47 +0100

    feat(widget): add FormDialog to widget exports for enhanced dialog handling

--- widgets/__init__.py
+++ widgets/__init__.py
@@ -1,1 +1,1 @@
-from ._widgets import Label, Box, Button, TextInput, LogDisplay, ProgressBar, Chart, List, Select, Checkbox, VStack, HStack, Frame, Dialog
+from ._widgets import Label, Box, Button, TextInput, LogDisplay, ProgressBar, Chart, List, Select, Checkbox, VStack, HStack, Frame, Dialog, FormDialog
